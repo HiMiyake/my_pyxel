9cb42e80d87566b5c3c3a11dfa7aca346dc26555
H.Miyake <example@example.com>
2026-09-24 21:51:32 +0900

    バーチャルパッドに対応

--- main.py
+++ main.py
@@ -5,7 +5,9 @@ import pyxel
 GAME_TITLE = "Space Rescue"
 SHIP_SPEED = 2
 SCREEN_WIDTH = 160
+SCREEN_HEIGHT = 120
 SHIP_WIDTH = 8
+SHIP_HEIGHT = 8
 
 
 class App:
@@ -21,12 +23,20 @@ class App:
         if pyxel.btnp(pyxel.KEY_Q):
             pyxel.quit()
 
-        if pyxel.btn(pyxel.KEY_LEFT):
+        if (pyxel.btn(pyxel.KEY_LEFT)
+                or pyxel.btn(pyxel.GAMEPAD1_BUTTON_DPAD_LEFT)):
             self.ship_x -= SHIP_SPEED
-        if pyxel.btn(pyxel.KEY_RIGHT):
+        if (pyxel.btn(pyxel.KEY_RIGHT)
+                or pyxel.btn(pyxel.GAMEPAD1_BUTTON_DPAD_RIGHT)):
             self.ship_x += SHIP_SPEED
+        if pyxel.btn(pyxel.KEY_UP) or pyxel.btn(pyxel.GAMEPAD1_BUTTON_DPAD_UP):
+            self.ship_y -= SHIP_SPEED
+        if (pyxel.btn(pyxel.KEY_DOWN)
+                or pyxel.btn(pyxel.GAMEPAD1_BUTTON_DPAD_DOWN)):
+            self.ship_y += SHIP_SPEED
 
         self.ship_x = max(0, min(self.ship_x, SCREEN_WIDTH - SHIP_WIDTH))
+        self.ship_y = max(0, min(self.ship_y, SCREEN_HEIGHT - SHIP_HEIGHT))
 
     def draw(self):
         pyxel.cls(0)
